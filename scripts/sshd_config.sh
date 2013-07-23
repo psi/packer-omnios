8a6e86705ce6cff40f06ee83fe315023d5d126b3
JD Harrington <jd@jdharrington.net>
2013-07-23 00:00:49 -0400

    Fix sshd_config for SunSSH

--- scripts/sshd_config.sh
+++ scripts/sshd_config.sh
@@ -1,2 +1,6 @@
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-sed -i 's/UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
+
+cat <<EOF >> /etc/ssh/sshd_config
+
+LookupClientHostnames no
+EOF
