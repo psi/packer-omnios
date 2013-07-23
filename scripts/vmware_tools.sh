dc5e2825416d959926d2635960a2aafa040d75b9
JD Harrington <jd@jdharrington.net>
2013-07-22 22:30:13 -0400

    Install VMWare Tools

--- scripts/vmware_tools.sh
+++ scripts/vmware_tools.sh
@@ -1,5 +1,19 @@
-cp /cdrom/vmwaretools/vmware-solaris-tools.tar.gz /tmp
-cd /tmp
-gunzip vmware-solaris-tools.tar.gz
-tar xvf vmware-solaris-tools.tar
+# Mount the tools ISO
+lofiadm -a /root/solaris.iso /dev/lofi/1
+mount -F hsfs -o ro /dev/lofi/1 /mnt
 
+# Extract the installer
+cd /tmp
+tar xpzf /mnt/vmware-solaris-tools.tar.gz
+
+# The installer needs access to /mnt, so free it up
+umount /mnt
+lofiadm -d /dev/lofi/1
+
+# Install
+cd vmware-tools-distrib
+./vmware-install.pl --default
+
+# Clean up our mess
+cd /
+rm -rf /tmp/vmware-tools-distrib /root/solaris.iso
