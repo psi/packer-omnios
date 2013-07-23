868c237e651ca4fabfe304cf7a46a2e1ed22230f
JD Harrington <jd@jdharrington.net>
2013-07-22 22:37:33 -0400

    cleanup vagrant script

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -1,15 +1,10 @@
-# Turn on auto_home
-echo '*       localhost:/export/home/&' >> /etc/auto_home
-
 # Fix vagrant's homedir
 usermod -d /home/vagrant vagrant
 
 # Install vagrant's SSH pubkey
-VAGRANT_PUB_KEY='https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-
 mkdir -p /export/home/vagrant/.ssh
 chmod 700 /export/home/vagrant/.ssh
-curl $VAGRANT_PUB_KEY > /export/home/vagrant/.ssh/authorized_keys
+curl https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub > /export/home/vagrant/.ssh/authorized_keys
 chmod 600 /export/home/vagrant/.ssh/authorized_keys
 chown -R vagrant /export/home/vagrant/.ssh
 
